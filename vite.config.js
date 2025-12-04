eca04efb908b560eb8c5e3a3a3d3ba1f30ebb17b
Sujanraja <rsuji521@gmail.com>
2025-12-04 17:44:37 +0530

    Fix GitHub Pages build (use dist in docs)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sujandailyplannerfinal/', // GitHub repo name
+  base: '/sujandailyplannerfinal/',   // 👈 must match GitHub repo name
 });
